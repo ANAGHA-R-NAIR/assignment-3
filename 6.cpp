97b481750584d8a7dc0805b674b21562925cbdc3
ANAGHA-R-NAIR <31248818+ANAGHA-R-NAIR@users.noreply.github.com>
2017-09-06 19:04:53 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+int a;
+cout<<"Enter year= ";
+cin>>a;
+if( a%4==0) {
+cout<<"Leap year"<<endl;
+}else{
+cout<<"Not leap yeap"<<endl;
+}
+return 0;
+}
