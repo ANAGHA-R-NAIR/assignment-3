f55997e6481a886e8f5cc145189d032515125880
ANAGHA-R-NAIR <31248818+ANAGHA-R-NAIR@users.noreply.github.com>
2017-09-08 18:08:14 +0530

    Add files via upload

---
+++ 23.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+int a,n;
+a=1;
+cout<<"Enter value of n=";
+cin>>n;
+while(n>=a){
+cout<<n<<endl;
+n=n-1;
+}
+}
---
+++ 24.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+char a='a',i;
+while(i<='z')
+{
+cout<<i<<endl;
+i=a++;
+}
+}
