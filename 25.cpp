6729c16a3db8e4e849b402d789abf2327bae6bad
ANAGHA-R-NAIR <31248818+ANAGHA-R-NAIR@users.noreply.github.com>
2017-09-11 12:29:00 +0530

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+using namespace std;
+main(){
+int i=1;
+while(i<=100)
+{
+i=i+1;
+cout<<i<<endl;
+i++;
+}
+return 0;
+}
---
+++ 26.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main(){
+int i=0;
+while(i<=99)
+{
+i=i+1;
+cout<<i<<endl;
+i++;
+}
+return 0;
+}
